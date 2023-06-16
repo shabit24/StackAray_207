071bcd6912edb167ca836105e6e1945538a9e198
shabit24 <hossain.shabit24@gmail.com>
2023-06-16 10:50:01 +0700

    return element

--- Stack.cpp
+++ Stack.cpp
@@ -20,7 +20,17 @@ public:
 			cout << "Number of data exceeds the limit "<<endl;
 			return 0;
 		}
+		top++;
+		stack_array[top] = element;
+		cout << endl;
+		cout << element << "ditambahkan(pushed)" << endl;
+		return element;
 
+	}
+	void pop() {
+		if (empty()) {
+
+		}
 	}
 
 
